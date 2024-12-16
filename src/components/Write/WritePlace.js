0bb7e0a2cbb796039a3737c52d5e1405cb297445
JeongHwanjun <ghkswnsdlwjd@naver.com>
2024-12-16 09:11:48 +0900

    1216 - 이미지 업로드드

--- src/components/Write/WritePlace.js
+++ src/components/Write/WritePlace.js
@@ -8,7 +8,7 @@ const WritePlace = (props) => {
         '',
         'AI.jpg',
         'CentralLibrary.jpg',
-        'Gachon.jpg',
+        'GachonHall.jpg',
         'Pascucci.jpg',
         'StarBucks.jpg'
     ]
